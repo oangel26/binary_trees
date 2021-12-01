75c51f6adb2a67357f623fae1834842764f5fe3b
oangel26 <oa.angel26@gmail.com>
2021-12-01 14:32:49 -0500

    syntax error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	left_tree_node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
-		perent->left->parent = left_tree_node;
+		parent->left->parent = left_tree_node;
 		left_tree_node->left = parent->left;
 	}
 	parent->left = left_tree_node;
